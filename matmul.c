46c209af5c0194993be648514635f1bdfa417421
Artem Vassiliev <tema8@yahoo.com>
2013-04-27 20:17:13 -0700

    Clean Blocking code.

--- matmul.c
+++ matmul.c
@@ -4,21 +4,18 @@
 
 #include <xmmintrin.h>
 
-
-#define min(a,b) (((a)<(b))?(a):(b))
 #define MAX_BLOCK 32
+//getconf LEVEL1_DCACHE_LINESIZE
+#define CACHE_LINE (64/sizeof(double))
 
-static void matmul_blocking(int N, const double* A, const double* B, double* restrict C, int BLOCK);
-inline static void matmul_2x2(int N, const double* A, const double* B, double* restrict C);
-inline static void matmul_sse(int Stride, int N, const double* A, const double* B, double* restrict C);
+static void matmul_blocking(int N, const double* A, const double* B, double * restrict C);
+inline static void matmul_sse(int N, const double* A, const double* B, double * restrict C);
 
 void matmul(int N, const double* A, const double* B, double* restrict C) {
-  if(N==2)
-    return matmul_2x2(2, A, B, C);
-  else if(N<=MAX_BLOCK)
-    return matmul_sse(0, N, A, B, C);
+  if(N<=MAX_BLOCK)
+    return matmul_sse(N, A, B, C);
   else {
-    return matmul_blocking(N,A,B,C,min(N,MAX_BLOCK));
+    return matmul_blocking(N,A,B,C);
   }
 
   /*  int i, j, k;
@@ -30,168 +27,50 @@ void matmul(int N, const double* A, const double* B, double* restrict C) {
   */
 }
 
-void matmul_blocking(int N, const double* A, const double* B, double* restrict C, int BLOCK) {
-  int i, j, k, jj, kk;
-  double temp;
-  double temp_a[2];
-   __m128d a, b, c, c1;
 
-  for (jj = 0; jj < N; jj+=BLOCK){
-    //printf("jj %d\n",jj);
-
-    for (kk = 0; kk < N; kk+=BLOCK){
-      //printf("  kk %d\n",kk);
-
-      for (i=0;i<N;i+=2){
-	//printf("    i %d\n",i);
-
-	//printf("=== A[%d*%d+%d], B[%d*%d+%d], C[%d*%d+%d] \n", jj,N,kk,kk,N,i, jj,N, i);
-
-	//matmul_sse(N, BLOCK, &A[jj*N+kk], &B[kk*N+i], &C[jj*N + i]);
-
-	for(j=jj;j<=min(jj+BLOCK-1,N);j++){
-	  temp = 0.;
-	  //printf("## C[%d*%d+%d] ",j,N,i);
-	  //printf(" (%p, %d) ", &C[j*N+i], ((long long)( &C[j*N+i]))%16);
-	  c1 = _mm_load_pd(&C[j*N+i]);
-	  //printf(" ## \n");
-
-	  for ( k= kk; k <=min(kk+BLOCK-1,N); k++){
-	    a  = _mm_set1_pd(A[j*(N)+k]);
-	    b  = _mm_load_pd(&B[k*(N)+i]);
-	    c  = _mm_mul_pd(a, b);
-	    c1 = _mm_add_pd(c, c1);
-
-	    //temp += A[j*N+k]*B[k*N+i];
+void matmul_blocking(int N, const double *A, const double *B, double * restrict C) {
+  for(int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j+=CACHE_LINE) {
+      for (int k = 0; k < N; k+=CACHE_LINE) {
+	for(int t = 0;t <CACHE_LINE;t++){
+	  for(int l = 0; l<CACHE_LINE;l++  ){
+	    C[i*N+j+l] += A[(i)*N+k+t] * B[(k+t)*N+j+l];
+	    //printf("%d,%d   C[%d][%d] +=  A[%d][%d]*B[%d][%d]     ",t,l  , i, j+l,    i, k+t, k+t, j+l);
 	  }
-	  //C[j*N + i] += temp;
-	  _mm_store_pd(&C[j*(N)+i], c1);
 	}
-
-
-	/*
-   for(int i =0; i<N;i++){
-     for (int j = 0; j < N; j+=2){
-       c1 = _mm_load_pd(&C[i*(N+Stride)+j]);
-
-       for(int k=0;k<N;k++){
-	 a  = _mm_set1_pd(A[i*(N+Stride)+k]);
-	 b  = _mm_load_pd(&B[k*(N+Stride)+j]);
-	 c  = _mm_mul_pd(a, b);
-	 c1 = _mm_add_pd(c, c1);
-       }
-       _mm_store_pd(&C[i*(N+Stride)+j], c1);
-     }
-   }*/
-
-
-	/*for(j=jj;j<=min(jj+BLOCK-1,N);j++){
-	  temp = 0.;
-	  for ( k= kk; k <=min(kk+BLOCK-1,N); k++){
-	    temp += A[j*N+k]*B[k*N+i];
-	  }
-	  C[j*N + i] += temp;
-	}*/
 
       }
     }
   }
 }
 
-//Unrolled 2x2 Matrix Mult
-void matmul_2x2_2(int N, const double* A, const double* B, double* restrict C) {
-  C[2*0+0] += A[2*0+0]*B[2*0+0] + A[2*0+1]*B[2*1+0] ;
-  C[2*0+1] += A[2*0+0]*B[2*0+1] + A[2*0+1]*B[2*1+1] ;
-  C[2*1+0] += A[2*1+0]*B[2*0+0] + A[2*1+1]*B[2*1+0] ;
-  C[2*1+1] += A[2*1+0]*B[2*0+1] + A[2*1+1]*B[2*1+1] ;
-}
-
-//Unrolled 2x2 Matrix Mult with SSE
-inline static void matmul_2x2(int N, const double* A, const double* B, double* restrict C) {
-  __m128d a0, a, a1, b, b1, c, c1, c2;
-
-   //a = _mm_set1_pd(A[0]);
-	 a0  = _mm_load_pd(&A[0]);
-	 a = _mm_unpacklo_pd(a0,a0);
-  b = _mm_load_pd(&B[0]);
-  c = _mm_mul_pd(a, b);
-
-  //a1 = _mm_set1_pd(A[1]);
-	 a1 = _mm_unpackhi_pd(a0,a0);
-  b1 = _mm_load_pd(&B[2*1+0]);
-  c1 = _mm_mul_pd(a1, b1);
-
-  a = _mm_load_pd(&C[0]);
-  c2 = _mm_add_pd(c, c1);
-  c1 = _mm_add_pd(a, c2);
-  _mm_store_pd(&C[0], c1);
-
-
-  //a = _mm_set1_pd(A[2*1+0]);
-	 a0  = _mm_load_pd(&A[2*1+0]);
-	 a = _mm_unpacklo_pd(a0,a0);
-  c = _mm_mul_pd(a, b);
-
-  a1 = _mm_set1_pd(A[2*1+1]);
-  c1 = _mm_mul_pd(a1, b1);
-
-  //a = _mm_load_pd(&C[2*1+0]);
-	 a1 = _mm_unpackhi_pd(a0,a0);
-  c2 = _mm_add_pd(c, c1);
-  c1 = _mm_add_pd(a, c2);
-  _mm_store_pd(&C[2*1+0], c1);
-}
 
 //N has to be even
-//  for (i = 0; i < N; i++)
-//    for (j = 0; j < N; j++)
-//      for (k = 0; k < N; k++)
-//       C[i*N + j] += A[i*N+k]*B[k*N+j];
-
-
-inline static void matmul_sse(int Stride, int N, const double* A, const double* B, double* restrict C) {
-  __m128d a, a1, a2, b, c, c1;
-   for(int i =0; i<N;i++){
-     for (int j = 0; j < N; j+=2){
-       c1 = _mm_load_pd(&C[i*(N+Stride)+j]);
-
-       for(int k=0;k<N;k+=2){
-	 //a  = _mm_set1_pd(A[i*(N+Stride)+k]);
-	 a  = _mm_load_pd(&A[i*(N+Stride)+k]);
-	 a1 = _mm_unpacklo_pd(a,a);
-	 a2 = _mm_unpackhi_pd(a,a);
-
-	 b  = _mm_load_pd(&B[k*(N+Stride)+j]);
-	 c  = _mm_mul_pd(a1, b);
-	 c1 = _mm_add_pd(c, c1);
-
-	 b  = _mm_load_pd(&B[(k+1)*(N+Stride)+j]);
-	 c  = _mm_mul_pd(a2, b);
-	 c1 = _mm_add_pd(c, c1);
-
-       }
-       _mm_store_pd(&C[i*(N+Stride)+j], c1);
-     }
-   }
-
-}
-
-/*
 inline static void matmul_sse(int N, const double* A, const double* B, double* restrict C) {
-   __m128d a, b, c, c1;
+  __m128d a, a1, a2, b, c, c1;
    for(int i =0; i<N;i++){
      for (int j = 0; j < N; j+=2){
        c1 = _mm_load_pd(&C[i*N+j]);
 
-       for(int k=0;k<N;k++){
-	 a  = _mm_set1_pd(A[i*N+k]);
+       for(int k=0;k<N;k+=2){
+	 //a  = _mm_set1_pd(A[i*N+k]);
+	 a  = _mm_load_pd(&A[i*N+k]);
+	 a1 = _mm_unpacklo_pd(a,a);
+	 a2 = _mm_unpackhi_pd(a,a);
+
 	 b  = _mm_load_pd(&B[k*N+j]);
-	 c  = _mm_mul_pd(a, b);
+	 c  = _mm_mul_pd(a1, b);
 	 c1 = _mm_add_pd(c, c1);
+
+	 b  = _mm_load_pd(&B[(k+1)*N+j]);
+	 c  = _mm_mul_pd(a2, b);
+	 c1 = _mm_add_pd(c, c1);
+
        }
        _mm_store_pd(&C[i*N+j], c1);
      }
    }
 
 }
-*/
+
+
